5452ba9617976f59f264ecf805fb5bae26f8873b
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-01-30 12:05:04 -0300

    feat: project back-end created

---
+++ crech-au/src/main/java/com/api/crechau/CrechAuApplication.java
@@ -0,0 +1,13 @@
+package com.api.crechau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrechAuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrechAuApplication.class, args);
+	}
+
+}
